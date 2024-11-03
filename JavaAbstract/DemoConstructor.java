330aafecc6cc7e0aa97569b20a40929f087e2caa
amanpandey-ayla <aman.pandey@aylanetworks.com>
2024-11-03 21:52:38 +0530

    Constructor

---
+++ JavaAbstract/DemoConstructor.java
@@ -0,0 +1,16 @@
+package JavaAbstract;
+
+public class DemoConstructor {
+
+    DemoConstructor (){
+       System.out.println("Default constructor");
+    }
+
+
+    DemoConstructor (int a){
+        System.out.println("Prameterized constructor");
+    }
+}
+
+
+
---
+++ JavaAbstract/TestConstructor.java
@@ -0,0 +1,9 @@
+package JavaAbstract;
+
+public class TestConstructor{
+
+    public static void main(String[] args) {
+        DemoConstructor abc = new DemoConstructor();
+        DemoConstructor xyz = new DemoConstructor(23);
+    }
+}
